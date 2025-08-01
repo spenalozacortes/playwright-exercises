1363cc8334223318f15132fe002fa87d590188b4
Stephanie Penaloza <spenalozacortes@gmail.com>
2025-08-01 17:01:03 -0600

    add tags

--- tests/candyMapper/login.spec.ts
+++ tests/candyMapper/login.spec.ts
@@ -3,10 +3,12 @@ import { successMessages } from '../../test-data/commonData';
 
 const SUCCESS_MESSAGE = successMessages.login;
 
-test('Login to CandyMapper and assert user is logged in', async ({ loggedInLoginPage }) => {
-  expect(await loggedInLoginPage.getLoginMessage()).toContain(SUCCESS_MESSAGE);
-}); 
-
-test('Login to CandyMapper and assert user is not logged in', async ({ loggedInLoginPage }) => {
-  expect(await loggedInLoginPage.getLoginMessage()).not.toContain(SUCCESS_MESSAGE);
-}); 
+test.describe('smoke', () => {
+  test('Login to CandyMapper and assert user is logged in', async ({ loggedInLoginPage }) => {
+    expect(await loggedInLoginPage.getLoginMessage()).toContain(SUCCESS_MESSAGE);
+  }); 
+  
+  test('Login to CandyMapper and assert user is not logged in', async ({ loggedInLoginPage }) => {
+    expect(await loggedInLoginPage.getLoginMessage()).not.toContain(SUCCESS_MESSAGE);
+  }); 
+});
